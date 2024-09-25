4df1c9d6c66e5409989569bea16de4731758a4d4
mphanias <pmokrala@aerospike.com>
2024-09-25 22:11:12 +0530

    ip address

--- start_consul.sh
+++ start_consul.sh
@@ -1,1 +1,2 @@
-consul agent -client=10.140.0.89 -bind=10.140.0.89 -advertise=10.140.0.89 -log-level=err -dev > /var/log/consul.log
+#consul agent -client=10.140.0.89 -bind=10.140.0.89 -advertise=10.140.0.89 -log-level=err -dev > /var/log/consul.log
+consul agent -client=$GCP_IP_ADDR -bind=$GCP_IP_ADDR -advertise=$GCP_IP_ADDR -log-level=err -dev > /var/log/consul.log
