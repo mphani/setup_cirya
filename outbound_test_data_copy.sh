f59c1057f21456896ed23046db114a735bac24cc
mphanias <pmokrala@aerospike.com>
2024-09-25 22:26:35 +0530

    data copy

---
+++ outbound_test_data_copy.sh
@@ -0,0 +1,10 @@
+export BASE_FOLDER=/root
+export AEROSPIKE_FOLDER=$BASE_FOLDER/aerospike
+export BASE_TEST_DATA_FOLDER=$BASE_FOLDER//root/aerospike-test-data
+
+export OUTBOUND_KAFKA_TEST_DATA_FOLDER=$AEROSPIKE_FOLDER/aerospike-connect/kafka-outbound/src/test/data
+
+cp $BASE_REST_DATA_FOLDER/kafka/* $OUTBOUND_KAFKA_TEST_DATA_FOLDER/kafka
+cp $BASE_REST_DATA_FOLDER/zookeeper/* $OUTBOUND_KAFKA_TEST_DATA_FOLDER/zookeeper
+cp $BASE_REST_DATA_FOLDER/aerospike/* $OUTBOUND_KAFKA_TEST_DATA_FOLDER/aerospike
+
