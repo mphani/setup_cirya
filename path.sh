845531c676741d943a624fff474ffe374b37f48b
mphanias <pmokrala@aerospike.com>
2024-09-25 22:44:15 +0530

    path fix

--- path.sh
+++ path.sh
@@ -10,6 +10,6 @@ echo "Will use GCP Local IP Address in configs "$GCP_IP_ADDR
 export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64/
 export PATH=$JAVA_HOME/bin:$PATH
 
-export PATH=$BASE_FOLDER/env_scripts/setup_cirya:$PATH
+export PATH=$BASE_FOLDER/env_scripts:$PATH
 export PATH=$BASE_FOLDER/aerospike/cirya-3.3.15/bin:$PATH
 
