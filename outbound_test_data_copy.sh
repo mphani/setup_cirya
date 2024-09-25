148837d4e19801429cf08a26156392f0cff19230
mphanias <pmokrala@aerospike.com>
2024-09-25 22:30:07 +0530

    errors fix

--- outbound_test_data_copy.sh
+++ outbound_test_data_copy.sh
@@ -1,6 +1,6 @@
 export BASE_FOLDER=/root
 export AEROSPIKE_FOLDER=$BASE_FOLDER/aerospike
-export BASE_TEST_DATA_FOLDER=$BASE_FOLDER//root/aerospike-test-data
+export BASE_TEST_DATA_FOLDER=$BASE_FOLDER/aerospike-test-data
 
 export OUTBOUND_KAFKA_TEST_DATA_FOLDER=$AEROSPIKE_FOLDER/aerospike-connect/kafka-outbound/src/test/data
 
